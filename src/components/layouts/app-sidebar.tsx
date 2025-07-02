c2c5182a7f0d02270a3a69559c3e9e1c9cc6552a
beubax <tejasr@bu.edu>
2025-07-02 00:46:44 -0500

    fix: rename mcp-client/chatbot to wingmen throughout app

--- src/components/layouts/app-sidebar.tsx
+++ src/components/layouts/app-sidebar.tsx
@@ -72,7 +72,9 @@ export function AppSidebar() {
                     router.refresh();
                   }}
                 >
-                  <h4 className="font-bold">mcp-client/chatbot</h4>
+                  <h4 className="font-bold text-md leading-none text-center">
+                    Wingmen
+                  </h4>
                   <div
                     className="ml-auto block sm:hidden"
                     onClick={(e) => {
