078e8e03143c24cbd3582a59617a03aaabd3eaeb
beubax <tejasr@bu.edu>
2025-07-24 01:51:37 -0500

    test: change model to run accuracy test

--- src/lib/ai/models.ts
+++ src/lib/ai/models.ts
@@ -1,16 +1,16 @@
 // models.ts
 import { LanguageModel } from "ai";
-import { createOpenRouter } from "@openrouter/ai-sdk-provider";
+// import { createOpenRouter } from "@openrouter/ai-sdk-provider";
 import { openai } from "@ai-sdk/openai";
 
-const openrouter = createOpenRouter({
-  apiKey: process.env.OPENROUTER_API_KEY,
-});
+// const openrouter = createOpenRouter({
+//   apiKey: process.env.OPENROUTER_API_KEY,
+// });
 
 export const customModelProvider = {
   getModel: (): LanguageModel => {
     // TODO: use openrouter
-    return openrouter("anthropic/claude-sonnet-4");
+    return openai("gpt-4.1");
   },
   getTitleModel: (): LanguageModel => {
     return openai("gpt-4.1");
