da064f40dc65ea7cfc2f2d1dd1a6432050f73be3
Manoj Bajaj <manojbajaj95@gmail.com>
2025-07-25 09:48:48 +0530

    feat(model): switch to anthropic

--- src/lib/ai/models.ts
+++ src/lib/ai/models.ts
@@ -1,16 +1,17 @@
 // models.ts
 import { LanguageModel } from "ai";
-import { createOpenRouter } from "@openrouter/ai-sdk-provider";
+// import { createOpenRouter } from "@openrouter/ai-sdk-provider";
 import { openai } from "@ai-sdk/openai";
+import { anthropic } from "@ai-sdk/anthropic";
 
-const openrouter = createOpenRouter({
-  apiKey: process.env.OPENROUTER_API_KEY,
-});
+// const openrouter = createOpenRouter({
+//   apiKey: process.env.OPENROUTER_API_KEY,
+// });
 
 export const customModelProvider = {
   getModel: (): LanguageModel => {
     // TODO: use openrouter
-    return openrouter("anthropic/claude-sonnet-4");
+    return anthropic("claude-4-sonnet-20250514");
   },
   getTitleModel: (): LanguageModel => {
     return openai("gpt-4.1");
