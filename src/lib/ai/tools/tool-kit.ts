c146651b964440e0f9db3bdd3ab7f560b37148c6
beubax <tejasr@bu.edu>
2025-08-10 23:54:28 -0500

    feat: add table data visualization tool

--- src/lib/ai/tools/tool-kit.ts
+++ src/lib/ai/tools/tool-kit.ts
@@ -6,6 +6,7 @@ import { AppDefaultToolkit, DefaultToolName } from ".";
 import { Tool } from "ai";
 import { httpFetchTool } from "./http/fetch";
 import { pythonExecutionTool } from "./code/python-run-tool";
+import { createTableTool } from "./visualization/create-table";
 
 export const APP_DEFAULT_TOOL_KIT: Record<
   AppDefaultToolkit,
@@ -15,6 +16,7 @@ export const APP_DEFAULT_TOOL_KIT: Record<
     [DefaultToolName.CreatePieChart]: createPieChartTool,
     [DefaultToolName.CreateBarChart]: createBarChartTool,
     [DefaultToolName.CreateLineChart]: createLineChartTool,
+    [DefaultToolName.CreateTable]: createTableTool,
   },
   [AppDefaultToolkit.WebSearch]: {
     [DefaultToolName.WebSearch]: tavilySearchTool,
